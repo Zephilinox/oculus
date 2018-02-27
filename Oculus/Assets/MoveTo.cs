03e75d5c3d7ecca8af0b1e7d7378c25e3885c2ea
Ricardo Heath <ricardo2.heath@live.uwe.ac.uk>
2018-02-27 10:41:53 +0000

    initial

---
+++ Oculus/Assets/MoveTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        player = Camera.main.gameObject.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * speed);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(player.name);
+        if (collision.gameObject.name == player.name)
+        {
+            Debug.Log(collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Oculus/Assets/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public float spawnDelay;
+    public float timer;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > spawnDelay)
+        {
+            timer = 0;
+            GameObject gameobj = Instantiate(prefab);
+            gameobj.transform.position = transform.position;
+            gameobj.AddComponent<MoveTo>();
+            gameobj.GetComponent<MoveTo>().speed = speed;
+        }
+    }
+}
